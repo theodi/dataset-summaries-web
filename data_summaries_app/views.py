b4e8440429de517a0ddec8acaccc8ff406f073fa
Rachel Wilson <rachel.wilson@theodi.com>
2018-04-20 10:11:28 +0100

    Add hook for Emilia's data-summaries-scrpts

--- data_summaries_app/views.py
+++ data_summaries_app/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+import os,sys
+sys.path.insert(1, os.path.join(sys.path[0], './scripts'))
 
+from django.http import HttpResponse
+from scripts import pretend_i_am_data_summary_script
+
+
+def callScript():
+    return pretend_i_am_data_summary_script.summary()
 
 def homePageView(request):
-    return HttpResponse('Hello, World!')
+    return HttpResponse(callScript())
---
+++ scripts/pretend_i_am_data_summary_script.py
@@ -0,0 +1,3 @@
+#if __name__ == "__main__":
+def summary():
+    return "Aren't I a lovely summary"
